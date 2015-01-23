2f41b49e8d6ae2386f5e4d66105235b770455555
Artur Kordowski <artur.kordowski@gmail.com>
2015-01-23 09:03:49 +0100

    Add Template Method Design Pattern.

---
+++ 10_TemplateMethodPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Making tea ...");
+			Tea tea = new Tea();
+			tea.PrepareRecipe();
+
+			Console.WriteLine("\nMaking coffee ...");
+			Coffee coffee = new Coffee();
+			coffee.PrepareRecipe();
+
+			Console.ReadLine();
+		}
+	}
+}
